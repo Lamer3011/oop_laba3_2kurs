47725042f964ad0b9f753f37a1e154be6c98b0b6
Kireev <Kireev@DESKTOP-PF4DRIV>
2021-10-02 13:30:46 +0300

    Add project files.

---
+++ ex2/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.IO;
+
+namespace ex2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string s;
+            double x, y;
+
+            StreamWriter writer = new StreamWriter("D:\\visual studio projects\\oop_laba3_2kurs\\ex2\\laba2.res");
+            StreamReader reader = new StreamReader("D:\\visual studio projects\\oop_laba3_2kurs\\ex2\\laba2.txt");
+
+            writer.WriteLine("Отримано: \n");
+            metka: s = reader.ReadLine();
+            if (s == null) goto metka1;
+            x = double.Parse(s);
+            y = Math.Pow(Math.Sin(x), 2)/(x - 1);
+
+            writer.WriteLine("x = {0:F3} y = {1:e3} \n ************", x, y);
+            goto metka;
+            metka1: writer.WriteLine("Створив Сергій Кірєєв {0} \n", s);
+
+            writer.Close();
+            reader.Close();
+
+        }
+    }
+}
---
+++ oop_laba3_2kurs/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace oop_laba3_2kurs
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //variables
+            int a;
+            float i;
+            double c;
+            bool l;
+            string name;
+
+            //enter var's
+            Console.Write("Enter your name: ");
+            name = Console.ReadLine();
+            Console.Write("Enter a(int): ");
+            a = Convert.ToInt32(Console.ReadLine());
+            Console.Write("Enter i(float): ");
+            i = Convert.ToSingle(Console.ReadLine());
+            Console.Write("Enter c(double): ");
+            c = Convert.ToDouble(Console.ReadLine());
+            Console.Write("Enter l(bool): ");
+            l = Convert.ToBoolean(Console.ReadLine());
+
+            //formating
+            Console.WriteLine("Formating result: \n name = {0, 6}, l = {1, 4}", name, l);
+            Console.WriteLine("a = {0, 4}, i = {1, 10:f5}, c = {2, 20:e8}", a, i, c);
+            Console.WriteLine();
+        }
+    }
+}
